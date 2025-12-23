c48e1e67c75588421c3de12019c896a188be330a
snowmerak <snowmerak@outlook.com>
2025-12-24 01:08:26 +0900

    chore(utils): remove deprecated utility functions

--- utils.go
+++
@@ -1,9 +0,0 @@
-package falkordb
-
-import "github.com/snowmerak/falkordb-go/util/strs"
-
-// Deprecated: use util/strs.ToString.
-func ToString(i interface{}) string { return strs.ToString(i) }
-
-// Deprecated: use util/strs.RandomString.
-func RandomString(n int) string { return strs.RandomString(n) }
